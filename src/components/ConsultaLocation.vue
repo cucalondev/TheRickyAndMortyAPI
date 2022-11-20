27fd8b11981cb8a7f5862727c2c12b41cdc366d9
Aphans <rapperanqeel@hotmail.com>
2022-11-20 01:36:03 +0100

    A

--- src/components/ConsultaLocation.vue
+++ src/components/ConsultaLocation.vue
@@ -1,7 +1,18 @@
 <template>
         <ol>
         <li v-for="el of localidades" v-bind:key="el.id">
-          {{el.name}}
+          {{el.name}} 
+          <ol>
+            <li v-for="p of el.residents" v-bind:key="p.id">
+              {{conseguirPersonaje(p)}}
+            </li>
+          </ol>
+          </li>
+      </ol>
+      <label>Residentes</label>
+      <ol>
+        <li v-for="el of residentes" v-bind:key="el.id">
+           <p>Hola</p>
         </li>
       </ol>
       <button @click="siguientePagina(pagina),conseguirLocalidades(pagina)">Siguiente</button>
@@ -26,17 +37,30 @@ methods:{
       axios
       .get(`https://rickandmortyapi.com/api/location?page=${page}`)
         .then((localidades) => {
-          console.log(localidades);
           this.localidades=localidades.data.results;
         })
         .catch((e) => console.log(e))
         .finally(() => this.loading = false);
     },
+    conseguirPersonaje(personaje){
+      axios
+      .get(personaje)
+        .then((personaje) => {
+          console.log(personaje);
+          this.residentes=personaje.data.results;
+        })
+        .catch((e) => console.log(e))
+        .finally(() => this.loading = false);
+    },
+
     siguientePagina(page){
       this.pagina=page+1;
     },
     atrasPagina(page){
       this.pagina=page-1;
+    },
+    añadirResidentes(residentes){
+      this.residentes=residentes;
     }
     },
 }

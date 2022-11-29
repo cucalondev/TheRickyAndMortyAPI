603bbbb1d9ed6572afbdd125f9aebfefc78990df
Aphans <rapperanqeel@hotmail.com>
2022-11-29 09:21:43 +0100

    random generator

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/main.css";
+const app = createApp(App);
 
-createApp(App).mount('#app')
+
+app.mount("#app");
